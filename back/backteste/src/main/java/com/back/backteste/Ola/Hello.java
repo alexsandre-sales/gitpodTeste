6440d24359ebc5f0f44ca0e0f710b01950ae6219
Alex kogake <lexkogake@gmail.com>
2022-01-30 17:49:11 +0000

    Rodando java 11

--- back/backteste/src/main/java/com/back/backteste/Ola/Hello.java
+++ back/backteste/src/main/java/com/back/backteste/Ola/Hello.java
@@ -1,5 +1,12 @@
 package com.back.backteste.Ola;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Hello {
-    
+    @GetMapping(value="/")
+    public String getMethodName() {
+        return "<h1>Ola mundo Java</h1>";
+    }
 }
